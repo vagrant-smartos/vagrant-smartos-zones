201c871500aab0a11c47cf96bfee73378d9dca80
Eric Saxby <sax@livinginthepast.org>
2014-03-29 12:52:51 -0700

    Set vagrant pass, sudoer in local zone

--- lib/vagrant/smartos/zones/cap/create_zone_users.rb
+++ lib/vagrant/smartos/zones/cap/create_zone_users.rb
@@ -12,6 +12,10 @@ module Vagrant
 
             machine.communicate.execute("pfexec zlogin #{zone.uuid} useradd -m -s /bin/bash vagrant")
             machine.communicate.execute("pfexec zlogin #{zone.uuid} usermod -P\\'Primary Administrator\\' vagrant")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i -e \\'s@root:.*@root:\\$5\\$90x8mAeX\\$SKKNEjeztV.ruPBNf/E5y3xqGkwDv9A5KNP2S89GuB.:16151::::::@\\' /etc/shadow")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i -e \\'s@vagrant:.*@vagrant:\\$5\\$UzQ1rHU/\\$o67DYOyHOOabzJt.6DwgZP2qCGoAO7aVel2bgkuIwL7:16158::::::@\\' /etc/shadow")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} cp /opt/local/etc/sudoers.d/admin /opt/local/etc/sudoers.d/vagrant")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i '' \\'s@admin@vagrant@\\' /opt/local/etc/sudoers.d/vagrant")
           end
         end
       end
