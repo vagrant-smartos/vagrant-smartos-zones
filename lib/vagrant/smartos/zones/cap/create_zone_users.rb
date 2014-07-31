839cc0b6e9e7cff8cea312d32e02097a6d77ed32
Eric Saxby <sax@livinginthepast.org>
2014-07-31 13:54:14 -0700

    Create zone vagrant user in vagrant group

--- lib/vagrant/smartos/zones/cap/create_zone_users.rb
+++ lib/vagrant/smartos/zones/cap/create_zone_users.rb
@@ -10,7 +10,8 @@ module Vagrant
             zone = Util::ZoneInfo.new(machine).show(machine.config.zone.name)
             ui.info "Creating users in zone #{zone.name} #{zone.uuid}"
 
-            machine.communicate.execute("pfexec zlogin #{zone.uuid} useradd -m -s /bin/bash vagrant")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} groupadd vagrant")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} useradd -m -s /bin/bash -G vagrant vagrant")
             machine.communicate.execute("pfexec zlogin #{zone.uuid} usermod -P\\'Primary Administrator\\' vagrant")
             machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i -e \\'s@root:.*@root:\\$5\\$90x8mAeX\\$SKKNEjeztV.ruPBNf/E5y3xqGkwDv9A5KNP2S89GuB.:16151::::::@\\' /etc/shadow")
             machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i -e \\'s@vagrant:.*@vagrant:\\$5\\$UzQ1rHU/\\$o67DYOyHOOabzJt.6DwgZP2qCGoAO7aVel2bgkuIwL7:16158::::::@\\' /etc/shadow")
