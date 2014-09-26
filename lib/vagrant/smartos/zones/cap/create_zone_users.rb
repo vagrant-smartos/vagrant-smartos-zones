09bbf73a1834fd81ae634763e9a61a3cdee1f757
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-09-26 14:52:15 -0700

    Ensure vagrant RSA key gets written in local zone
    zlogin echo > file was executing the > in the gz

--- lib/vagrant/smartos/zones/cap/create_zone_users.rb
+++ lib/vagrant/smartos/zones/cap/create_zone_users.rb
@@ -7,6 +7,7 @@ module Vagrant
         class CreateZoneUsers
           def self.create_zone_users(machine)
             ui = machine.ui
+            sudo = machine.config.smartos.suexec_cmd
             zone = Util::ZoneInfo.new(machine).show(machine.config.zone.name)
             ui.info "Creating users in zone #{zone.name} #{zone.uuid}"
 
@@ -24,8 +25,10 @@ module Vagrant
             machine.communicate.execute("#{sudo} zlogin #{zone.uuid} sed -i -e \\'s@admin@vagrant@\\' /opt/local/etc/sudoers.d/vagrant")
             
             # add vagrant public key to authorized_keys
-            machine.communicate.execute("#{sudo} zlogin #{zone.uuid} echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' > /home/vagrant/.ssh/authorized_keys")
-            machine.communicate.execute("#{sudo} zlogin #{zone.uuid} chown vagrant:other /home/vagrant/.ssh/authorized_keys")
+            machine.communicate.execute("#{sudo} zlogin #{zone.uuid} mkdir -p /home/vagrant/.ssh")
+            machine.communicate.execute("#{sudo} zlogin #{zone.uuid} touch /home/vagrant/.ssh/authorized_keys")
+            machine.communicate.execute(%Q{#{sudo} zlogin #{zone.uuid} 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > /home/vagrant/.ssh/authorized_keys'})
+            machine.communicate.execute("#{sudo} zlogin #{zone.uuid} chown -R vagrant:other /home/vagrant/.ssh")
             machine.communicate.execute("#{sudo} zlogin #{zone.uuid} chmod 600 /home/vagrant/.ssh/authorized_keys")
           end
         end
