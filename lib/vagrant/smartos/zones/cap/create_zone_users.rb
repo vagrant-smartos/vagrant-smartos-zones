6920522b1037495ab8a91df60038c58e835f182d
Tyler Flint <tylerflint@gmail.com>
2014-09-26 10:51:48 -0600

    create_zone_users capability needs to add vagrant public key

--- lib/vagrant/smartos/zones/cap/create_zone_users.rb
+++ lib/vagrant/smartos/zones/cap/create_zone_users.rb
@@ -10,13 +10,23 @@ module Vagrant
             zone = Util::ZoneInfo.new(machine).show(machine.config.zone.name)
             ui.info "Creating users in zone #{zone.name} #{zone.uuid}"
 
+            # add vagrant user/group
             machine.communicate.execute("pfexec zlogin #{zone.uuid} groupadd vagrant")
             machine.communicate.execute("pfexec zlogin #{zone.uuid} useradd -m -s /bin/bash -G vagrant vagrant")
             machine.communicate.execute("pfexec zlogin #{zone.uuid} usermod -P\\'Primary Administrator\\' vagrant")
+
+            # set vagrant/root password to 'vagrant'
             machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i -e \\'s@root:.*@root:\\$5\\$90x8mAeX\\$SKKNEjeztV.ruPBNf/E5y3xqGkwDv9A5KNP2S89GuB.:16151::::::@\\' /etc/shadow")
             machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i -e \\'s@vagrant:.*@vagrant:\\$5\\$UzQ1rHU/\\$o67DYOyHOOabzJt.6DwgZP2qCGoAO7aVel2bgkuIwL7:16158::::::@\\' /etc/shadow")
+
+            # configure sudoers
             machine.communicate.execute("pfexec zlogin #{zone.uuid} cp /opt/local/etc/sudoers.d/admin /opt/local/etc/sudoers.d/vagrant")
-            machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i '' \\'s@admin@vagrant@\\' /opt/local/etc/sudoers.d/vagrant")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} sed -i -e \\'s@admin@vagrant@\\' /opt/local/etc/sudoers.d/vagrant")
+            
+            # add vagrant public key to authorized_keys
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' > /home/vagrant/.ssh/authorized_keys")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} chown vagrant:other /home/vagrant/.ssh/authorized_keys")
+            machine.communicate.execute("pfexec zlogin #{zone.uuid} chmod 600 /home/vagrant/.ssh/authorized_keys")
           end
         end
       end
