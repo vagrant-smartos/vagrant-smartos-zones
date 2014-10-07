e690253812300bcb630cb140358c0ca4bb942ae8
Eric Saxby <sax@livinginthepast.org>
2014-10-07 07:32:55 -0600

    WIP smartos communicator can talk to gz through gz_ssh port forward

--- lib/vagrant/smartos/zones/communicator/smartos.rb
+++ lib/vagrant/smartos/zones/communicator/smartos.rb
@@ -1,3 +1,5 @@
+require 'vagrant/smartos/zones/util/global_zone/connection'
+
 module Vagrant
   module Smartos
     module Zones
@@ -5,51 +7,74 @@ module Vagrant
         class Smartos < Vagrant.plugin("2").manager.communicators[:ssh]
           def initialize(machine)
             @machine = machine
-            @original_ssh_port = 2222
             machine.ui.info "hey there. I'm a smartos communicator"
             super
           end
-          # 
-          # def execute(command, opts=nil, &block)
-          #   @machine.ui.info 'ohai execute'
-          #   wrap_method do
-          #     @machine.ui.info 'wrapped'
-          #     @machine.ui.info @machine.ssh_info.inspect
-          #     super(command, opts, &block)
-          #   end
-          # end
-          #
-          # def sudo(command, opts=nil, &block)
-          #   @machine.ui.info 'ohai sudo'
-          #   wrap_method do
-          #     super(command, opts, &block)
-          #   end
-          # end
-          #
-          # def upload(from, to)
-          #   @machine.ui.info 'ohai upload'
-          #   wrap_method do
-          #     super(from, to)
-          #   end
-          # end
-          #
-          # def download(from, to=nil)
-          #   @machine.ui.info 'ohai download'
-          #   wrap_method do
-          #     super(from, to)
-          #   end
-          # end
-          #
-          # private
-          #
-          # def wrap_method
-          #   @machine.ui.info 'oh hey I got yr ssh port'
-          #   @machine.ui.info @machine.config.ssh.shell
-          #   @original_ssh_port = @machine.ssh_info[:port]
-          #   @machine.ssh_info[:port] = 2222
-          #   yield
-          #   @machine.ssh_info[:port] = @original_ssh_port
-          # end
+
+          def gz_execute(command, opts={}, &block)
+            opts = {
+              error_check: true,
+              error_class: Vagrant::Errors::VagrantError,
+              error_key:   :ssh_bad_exit_status,
+              good_exit:   0,
+              command:     command,
+              shell:       nil,
+              sudo:        false,
+            }.merge(opts)
+
+            opts[:good_exit] = Array(opts[:good_exit])
+
+            # Connect via SSH and execute the command in the shell.
+            stdout = ""
+            stderr = ""
+            global_zone_connector.connect
+
+            begin
+              generic_ssh_info = @connection_ssh_info
+              @connection_ssh_info = global_zone_connector.ssh_info
+
+              exit_status = global_zone_connector.connect do |connection|
+                shell_opts = {
+                  sudo: opts[:sudo],
+                  shell: opts[:shell],
+                }
+
+                shell_execute(connection, command, **shell_opts) do |type, data|
+                  if type == :stdout
+                    stdout += data
+                  elsif type == :stderr
+                    stderr += data
+                  end
+
+                  block.call(type, data) if block
+                end
+              end
+            ensure
+              @connection_ssh_info = generic_ssh_info
+            end
+
+            # Check for any errors
+            if opts[:error_check] && !opts[:good_exit].include?(exit_status)
+              # The error classes expect the translation key to be _key,
+              # but that makes for an ugly configuration parameter, so we
+              # set it here from `error_key`
+              error_opts = opts.merge(
+                _key: opts[:error_key],
+                stdout: stdout,
+                stderr: stderr
+              )
+              raise opts[:error_class], error_opts
+            end
+
+            # Return the exit status
+            exit_status
+          end
+
+          private
+
+          def global_zone_connector
+            @global_zone_connector ||= Vagrant::Smartos::Zones::Util::GlobalZone::Connection.new(@machine, @logger)
+          end
         end
       end
     end
---
+++ lib/vagrant/smartos/zones/util/global_zone/connection.rb
@@ -0,0 +1,199 @@
+require 'net/ssh'
+require 'stringio'
+require 'timeout'
+
+require 'vagrant/util/retryable'
+
+require 'vagrant/smartos/zones/util/global_zone/ssh_info'
+
+module Vagrant
+  module Smartos
+    module Zones
+      module Util
+        module GlobalZone
+          class Connection < Struct.new(:machine, :logger)
+
+            include Vagrant::Util::Retryable
+
+            CONNECTION_TIMEOUT = 60
+
+            # These are the exceptions that we retry because they represent
+            # errors that are generally fixed from a retry and don't
+            # necessarily represent immediate failure cases.
+            RETRYABLE_EXCEPTIONS = [
+              Errno::EACCES,
+              Errno::EADDRINUSE,
+              Errno::ECONNREFUSED,
+              Errno::ECONNRESET,
+              Errno::ENETUNREACH,
+              Errno::EHOSTUNREACH,
+              Net::SSH::Disconnect,
+              Timeout::Error
+            ]
+
+            class ErrorHandler < Struct.new(:error)
+              def handle!
+                case error.class
+                when Errno::EACCES
+                  # This happens on connect() for unknown reasons yet...
+                  raise Vagrant::Errors::SSHConnectEACCES
+                when Errno::ETIMEDOUT, Timeout::Error
+                  # This happens if we continued to timeout when attempting to connect.
+                  raise Vagrant::Errors::SSHConnectionTimeout
+                when Net::SSH::AuthenticationFailed
+                  # This happens if authentication failed. We wrap the error in our
+                  # own exception.
+                  raise Vagrant::Errors::SSHAuthenticationFailed
+                when Net::SSH::Disconnect
+                  # This happens if the remote server unexpectedly closes the
+                  # connection. This is usually raised when SSH is running on the
+                  # other side but can't properly setup a connection. This is
+                  # usually a server-side issue.
+                  raise Vagrant::Errors::SSHDisconnected
+                when Errno::ECONNREFUSED
+                  # This is raised if we failed to connect the max amount of times
+                  raise Vagrant::Errors::SSHConnectionRefused
+                when Errno::ECONNRESET
+                  # This is raised if we failed to connect the max number of times
+                  # due to an ECONNRESET.
+                  raise Vagrant::Errors::SSHConnectionReset
+                when Errno::EHOSTDOWN
+                  # This is raised if we get an ICMP DestinationUnknown error.
+                  raise Vagrant::Errors::SSHHostDown
+                when Errno::EHOSTUNREACH
+                  # This is raised if we can't work out how to route traffic.
+                  raise Vagrant::Errors::SSHNoRoute
+                when NotImplementedError
+                  # This is raised if a private key type that Net-SSH doesn't support
+                  # is used. Show a nicer error.
+                  raise Vagrant::Errors::SSHKeyTypeNotSupported
+                end
+              end
+            end
+
+            class SSHLogger
+              attr_reader :logger
+
+              def initialize
+                @io = StringIO.new
+                @logger = Logger.new(@io)
+              end
+
+              def to_s
+                @io.string
+              end
+            end
+
+            def with_connection
+              connect
+              yield @connection if block_given?
+            end
+
+            def connect(**opts)
+              if connection_valid?
+                return yield @connection if block_given?
+                return
+              end
+
+              validate_ssh_info!
+              check_ssh_key_permissions
+
+              # Default some options
+              opts[:retries] = 5 if !opts.has_key?(:retries)
+
+              # Connect to SSH, giving it a few tries
+              connection = nil
+              begin
+                logger.info("Attempting SSH connection...")
+                connection = retryable(tries: opts[:retries], on: RETRYABLE_EXCEPTIONS) do
+                  Timeout.timeout(CONNECTION_TIMEOUT) do
+                    begin
+                      ssh_logger = SSHLogger.new
+
+                      # Setup logging for connections
+                      connect_opts = common_connect_opts.dup
+                      connect_opts[:logger] = ssh_logger.logger
+
+                      if ssh_info[:proxy_command]
+                        connect_opts[:proxy] = Net::SSH::Proxy::Command.new(ssh_info[:proxy_command])
+                      end
+
+                      logger.info("Attempting to connect to SSH...")
+                      logger.info("  - Host: #{ssh_info[:host]}")
+                      logger.info("  - Port: #{ssh_info[:port]}")
+                      logger.info("  - Username: #{ssh_info[:username]}")
+                      logger.info("  - Password? #{!!ssh_info[:password]}")
+                      logger.info("  - Key Path: #{ssh_info[:private_key_path]}")
+
+                      Net::SSH.start(ssh_info[:host], ssh_info[:username], connect_opts)
+                    ensure
+                      # Make sure we output the connection log
+                      logger.debug("== Net-SSH connection debug-level log START ==")
+                      logger.debug(ssh_logger.to_s)
+                      logger.debug("== Net-SSH connection debug-level log END ==")
+                    end
+                  end
+                end
+              rescue StandardError => e
+                ErrorHandler.new(e).handle!
+              end
+
+              @connection = connection
+            end
+
+            def ssh_info
+              @ssh_info ||= Vagrant::Smartos::Zones::Util::GlobalZone::SSHInfo.new(machine.provider, machine.config, machine.env).to_hash
+            end
+
+            private
+
+            def check_ssh_key_permissions
+              ssh_info[:private_key_path].each do |path|
+                Vagrant::Util::SSH.check_key_permissions(Pathname.new(path))
+              end
+            end
+
+            def common_connect_opts 
+              # Build the options we'll use to initiate the connection via Net::SSH
+              @common_connect_opts ||= {
+                auth_methods:          ["none", "publickey", "hostbased", "password"],
+                config:                false,
+                forward_agent:         ssh_info[:forward_agent],
+                keys:                  ssh_info[:private_key_path],
+                keys_only:             true,
+                paranoid:              false,
+                password:              ssh_info[:password],
+                port:                  ssh_info[:port],
+                timeout:               15,
+                user_known_hosts_file: [],
+                verbose:               :debug,
+              }
+            end
+
+            def connection_valid?
+              return false unless @connection
+              return false if @connection.closed?
+
+              # There is a chance that the socket is closed despite us checking
+              # 'closed?' above. To test this we need to send data through the
+              # socket.
+              begin
+                @connection.exec!("")
+                true
+              rescue StandardError => e
+                logger.info("Connection errored, not re-using. Will reconnect.")
+                logger.debug(e.inspect)
+                @connection = nil
+                false
+              end
+            end
+
+            def validate_ssh_info!
+              raise Vagrant::Errors::SSHNotReady if ssh_info.nil?
+            end
+          end
+        end
+      end
+    end
+  end
+end
