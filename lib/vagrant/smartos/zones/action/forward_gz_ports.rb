ca43465734bf918c3e90ea41fee57c631ca9266a
Eric Saxby <sax@livinginthepast.org>
2014-10-08 17:52:33 -0600

    the Global Zone port forward runs through collision detection
    Turns out the Vagrant provides a built-in way to register new
    port forwards. By running this code early enough, it shows up in
    the list of port forwards that Vagrant will automatically correct
    in case of port conflicts.
    
    - use VMConfig#network to register gz_ssh port forward, instead of
      hacking env[:forwarded_ports]
    - [resolves #5]

--- lib/vagrant/smartos/zones/action/forward_gz_ports.rb
+++ lib/vagrant/smartos/zones/action/forward_gz_ports.rb
@@ -13,37 +13,23 @@ module Vagrant
             @env = env
           end
 
-          # This is probably dangerous. This runs *after* Vagrant
-          # checks for port collisions.
-          #   * this has to run before the guest is running
-          #   * we can't know if it's smartos until the guest is running
-          #   * we can only guess, because a zone is configured via this plugin
-          #
-          # Maybe we'll need to set up more complicated branching logic throughout
-          # the plugin. If there is a local zone configured, then we will have a
-          # gz_ssh port forward. Then when talking to the guest, we'll have to 
-          # choose between gz_ssh and ssh depending on certain conditions.
-          #
-          # This should definitely run through the port collision detection, and
-          # then communication should be dynamic based on the runtime info of this
-          # port forward.
+          # This middleware sets up a VirtualBox port forward to 2222 in the global zone.
+          # ipnat in the global zone is configured to route this to 22 in the global zone,
+          # even when all other ports are forwarded to a zone.
           def call(env)
 
             if zone_configured?
               machine.ui.info "Configuring a port forward to talk to the Global Zone"
 
-              env[:forwarded_ports] ||= compile_forwarded_ports(env[:machine].config)
-
               network = machine.config.vm.networks.first
               options = scoped_hash_override(network[1], :virtualbox)
 
-              gz_forward = VagrantPlugins::ProviderVirtualBox::Model::ForwardedPort.new(
-                'gz_ssh',
-                machine.config.global_zone.forwarded_ssh_port,
-                machine.config.global_zone.ssh_port,
-                options
-              )
-              env[:forwarded_ports] << gz_forward
+              machine.config.vm.network :forwarded_port, options.merge({
+                id: 'gz_ssh',
+                guest: machine.config.global_zone.ssh_port,
+                host: machine.config.global_zone.forwarded_ssh_port,
+                auto_correct: true
+              })
             end
 
             app.call(env)
