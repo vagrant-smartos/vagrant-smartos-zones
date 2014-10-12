469020614b57d979880dc50514a16aaeb9113cc5
Eric Saxby <sax@livinginthepast.org>
2014-10-12 12:26:44 -0600

    Clean up zone_info slightly
    TODO
    - sometimes this returns a Zone instance, sometimes an array of arrays.
    - should some of this logic move into Models::Zone?
    - #create logic should move here out of :zone_create capability, and
      include user creation

--- lib/vagrant/smartos/zones/models/zone.rb
+++ lib/vagrant/smartos/zones/models/zone.rb
@@ -5,6 +5,16 @@ module Vagrant
         class Zone
           attr_accessor :name, :state, :uuid, :brand, :image
 
+          def self.from_hash(zone)
+            new.tap do |z|
+              z.name = zone['alias']
+              z.uuid = zone['uuid']
+              z.brand = zone['brand']
+              z.state = zone['state']
+              z.image = zone['image_uuid']
+            end
+          end
+
           def running?
             state == 'running'
           end
--- lib/vagrant/smartos/zones/util/zone_info.rb
+++ lib/vagrant/smartos/zones/util/zone_info.rb
@@ -13,7 +13,7 @@ module Vagrant
 
           def list
             zones = []
-            machine.communicate.gz_execute("#{sudo} vmadm lookup -j") do |_type, output|
+            with_gz("#{sudo} vmadm lookup -j") do |output|
               zone_data = JSON.parse(output)
               zone_data.each do |zone|
                 zones << [zone['alias'].to_s.ljust(25),
@@ -34,7 +34,7 @@ module Vagrant
 
           def destroy(name)
             zone = show(name)
-            machine.communicate.gz_execute("#{sudo} vmadm delete #{zone.uuid}")
+            with_gz("#{sudo} vmadm delete #{zone.uuid}")
             zone.state = 'deleted'
             zone
           end
@@ -42,33 +42,35 @@ module Vagrant
           def show(name)
             zone_name = name || machine.config.zone.name
             zone = {}
-            machine.communicate.gz_execute("#{sudo} vmadm lookup -j -o uuid,alias,state,image_uuid,brand alias=#{zone_name}") do |_type, output|
+            with_gz("#{sudo} vmadm lookup -j -o uuid,alias,state,image_uuid,brand alias=#{zone_name}") do |output|
               zone.merge!(JSON.parse(output).first)
             end
 
-            Models::Zone.new.tap do |z|
-              z.name = zone['alias']
-              z.uuid = zone['uuid']
-              z.brand = zone['brand']
-              z.state = zone['state']
-              z.image = zone['image_uuid']
-            end
+            Models::Zone.from_hash(zone)
           end
 
           def start(name)
             zone = show(name)
-            machine.communicate.gz_execute("#{sudo} vmadm start #{zone.uuid}")
+            with_gz("#{sudo} vmadm start #{zone.uuid}")
             zone
           end
 
           def stop(name)
             zone = show(name)
-            machine.communicate.gz_execute("#{sudo} vmadm stop #{zone.uuid}")
+            with_gz("#{sudo} vmadm stop #{zone.uuid}")
             zone
           end
 
+          private
+
           def sudo
             machine.config.smartos.suexec_cmd
+          end
+
+          def with_gz(command)
+            machine.communicate.gz_execute(command) do |_type, output|
+              yield output if block_given?
+            end
           end
         end
       end
