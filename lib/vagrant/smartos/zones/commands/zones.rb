2960f7c3c11b3c1c174d076b637f787a97667476
Eric Saxby <sax@livinginthepast.org>
2014-10-11 19:36:36 -0600

    Refactor zones command
    Who owns what code? This still needs to be disentangled,
    but maybe this is a bit better.

--- lib/vagrant/smartos/zones/commands/zones.rb
+++ lib/vagrant/smartos/zones/commands/zones.rb
@@ -6,135 +6,116 @@ module Vagrant
     module Zones
       module Command
         class Zones < Vagrant.plugin('2', :command)
+          COMMANDS = %w(create destroy list show start stop).freeze
+
+          OPTION_PARSER = OptionParser.new do |o|
+            o.banner = 'Usage: vagrant zones [command] [name]'
+            o.separator ''
+            o.separator 'Commands:'
+            o.separator '  list             show status of zones'
+            o.separator '  create [name]    create or update zone with alais [name]'
+            o.separator '  destroy [name]   delete zone with alais [name]'
+            o.separator '  show [name]      show info on zone with alias [name]'
+            o.separator '  start [name]     start zone with alais [name]'
+            o.separator '  stop [name]      stop zone with alais [name]'
+            o.separator ''
+            o.separator 'Options:'
+            o.separator ''
+          end
+
           def self.synopsis
             'View and interact with SmartOS zones'
           end
 
           def execute
-            options = {}
-
-            opts = OptionParser.new do |o|
-              o.banner = 'Usage: vagrant zones [command] [name]'
-              o.separator ''
-              o.separator 'Commands:'
-              o.separator '  list             show status of zones'
-              o.separator '  create [name]    create or update zone with alais [name]'
-              o.separator '  destroy [name]   delete zone with alais [name]'
-              o.separator '  show [name]      show info on zone with alias [name]'
-              o.separator '  start [name]     start zone with alais [name]'
-              o.separator '  stop [name]      stop zone with alais [name]'
-              o.separator ''
-              o.separator 'Options:'
-              o.separator ''
-            end
-
-            argv = parse_options(opts)
+            argv = parse_options(OPTION_PARSER)
             return unless argv
 
-            case argv.shift
-            when 'list'
-              list
-            when 'create'
-              create argv.first
-            when 'destroy'
-              destroy argv.first
-            when 'show'
-              show argv.first
-            when 'start'
-              start argv.first
-            when 'stop'
-              stop argv.first
-            else
-              @env.ui.warn opts.to_s, prefix: false
+            command = argv.shift
+            command_method = COMMANDS.find { |c| c == command }
+
+            unless command_method
+              @env.ui.warn OPTION_PARSER.to_s, prefix: false
               exit 1
             end
+
+            send command_method, argv.first
           end
 
           def create(name)
-            with_target_vms('default') do |machine|
-              zone = Util::ZoneInfo.new(machine).create(name)
-              if zone
-                @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.create',
-                                    name: zone.name,
-                                    state: zone.state,
-                                    uuid: zone.uuid,
-                                    brand: zone.brand,
-                                    image: zone.image),
-                             prefix: false)
-              end
+            zones.create(name).tap do |zone|
+              return unless zone
+              @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.create',
+                                  name: zone.name,
+                                  state: zone.state,
+                                  uuid: zone.uuid,
+                                  brand: zone.brand,
+                                  image: zone.image),
+                           prefix: false)
             end
           end
 
           def destroy(name)
-            @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.destroy',
-                                name: name))
+            @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.destroy', name: name))
 
-            zone = nil
-            with_target_vms('default') do |machine|
-              zone = Util::ZoneInfo.new(machine).destroy(name)
+            zones.destroy(name).tap do |zone|
+              @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.destroyed',
+                                  name: zone.name,
+                                  state: zone.state,
+                                  uuid: zone.uuid,
+                                  brand: zone.brand,
+                                  image: zone.image))
             end
-
-            @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.destroyed',
-                                name: zone.name,
-                                state: zone.state,
-                                uuid: zone.uuid,
-                                brand: zone.brand,
-                                image: zone.image))
           end
 
-          def list
-            zones = []
-
-            with_target_vms('default') do |machine|
-              zones.concat(Util::ZoneInfo.new(machine).list)
+          def list(*_args)
+            zones.list.tap do |zone_list|
+              @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.list',
+                                  zones: zone_list.join("\n")),
+                           prefix: false)
             end
-
-            @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.list',
-                                zones: zones.join("\n")),
-                         prefix: false)
           end
 
           def show(name)
-            zone = nil
-            with_target_vms('default') do |machine|
-              zone = Util::ZoneInfo.new(machine).show(name)
+            zones.show(name).tap do |zone|
+              @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.show',
+                                  name: zone.name,
+                                  state: zone.state,
+                                  uuid: zone.uuid,
+                                  brand: zone.brand,
+                                  image: zone.image),
+                           prefix: false)
             end
-
-            @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.show',
-                                name: zone.name,
-                                state: zone.state,
-                                uuid: zone.uuid,
-                                brand: zone.brand,
-                                image: zone.image),
-                         prefix: false)
           end
 
           def start(name)
-            zone = nil
-            with_target_vms('default') do |machine|
-              zone = Util::ZoneInfo.new(machine).start(name)
+            zones.start(name).tap do |zone|
+              @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.start',
+                                  name: zone.name,
+                                  state: zone.state,
+                                  uuid: zone.uuid,
+                                  brand: zone.brand,
+                                  image: zone.image))
             end
-
-            @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.start',
-                                name: zone.name,
-                                state: zone.state,
-                                uuid: zone.uuid,
-                                brand: zone.brand,
-                                image: zone.image))
           end
 
           def stop(name)
-            zone = nil
-            with_target_vms('default') do |machine|
-              zone = Util::ZoneInfo.new(machine).stop(name)
+            zones.stop(name).tap do |zone|
+              @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.stop',
+                                  name: zone.name,
+                                  state: zone.state,
+                                  uuid: zone.uuid,
+                                  brand: zone.brand,
+                                  image: zone.image))
             end
+          end
 
-            @env.ui.info(I18n.t('vagrant.smartos.zones.commands.zones.stop',
-                                name: zone.name,
-                                state: zone.state,
-                                uuid: zone.uuid,
-                                brand: zone.brand,
-                                image: zone.image))
+          private
+
+          def zones
+            zones = nil
+            with_target_vms('default') { |machine| zones = Util::ZoneInfo.new(machine) }
+            zones
           end
         end
       end
